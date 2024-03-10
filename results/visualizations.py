3fdbedc04a72d75d9fb2b2e572b916f87dbf2c98
aespogom <100383119@alumnos.uc3m.es>
2024-03-10 16:14:04 +0100

    PH 120 results MLP joint

--- results/visualizations.py
+++ results/visualizations.py
@@ -3,27 +3,37 @@ from pickle import load
 from matplotlib.pylab import plt
 from numpy import arange
 
-parent_folder = input("Which is your student model? [MLP_parallel, MLP_tree, MLP_joint, MLP_scaled]")
+parent_folder = input("Which is your student model? [MLP_parallel, MLP_tree, MLP_joint, MLP_scaled]: ")
 assert parent_folder in ["MLP_parallel", "MLP_tree", "MLP_joint", "MLP_scaled"]
 
-date_exp = input("What is the date of the experiment (%YYYY-MM-DD)")
+train_type = input("Which training (causal, regular) :")
+assert train_type in ["causal", "regular"]
+
+date_exp = input("What is the date of the experiment (%YYYY-MM-DD): ")
 
 pred_h = input("What is your PH [30,45,60,120]")
 assert pred_h in ["30","45","60", "120"]
 # Load the training and validation loss dictionaries
-train_loss = load(open(os.path.join('results',parent_folder,f's_{parent_folder}_t_simglucose_data_insilico_seed_56_{date_exp}_PH_{pred_h}','train_loss.pkl'), 'rb'))
-val_loss = load(open(os.path.join('results',parent_folder,f's_{parent_folder}_t_simglucose_data_insilico_seed_56_{date_exp}_PH_{pred_h}','ii_loss.pkl'), 'rb'))
+if train_type == "causal":
+    val_loss = load(open(os.path.join('results',parent_folder,f's_{parent_folder}_t_simglucose_data_insilico_seed_56_{date_exp}_PH_{pred_h}','ii_loss.pkl'), 'rb'))
+    train_loss = load(open(os.path.join('results',parent_folder,f's_{parent_folder}_t_simglucose_data_insilico_seed_56_{date_exp}_PH_{pred_h}','train_loss.pkl'), 'rb'))
+    assert len(train_loss)==len(val_loss)
+else:
+    train_loss = load(open(os.path.join('results',parent_folder,f's_{parent_folder}_data_insilico_seed_56_{date_exp}_PH_{pred_h}','train_loss.pkl'), 'rb'))
 
-assert len(train_loss)==len(val_loss)
 # Generate a sequence of integers to represent the epoch numbers
 epochs = range(1, len(train_loss)+1)
  
 # Plot and label the training and ii loss values
 plt.plot(epochs, train_loss, label='Training Loss')
-plt.plot(epochs, val_loss, label='II Loss',alpha=0.5)
+if train_type == "causal":
+    plt.plot(epochs, val_loss, label='II Loss',alpha=0.5)
  
-# Add in a title and axes labels
-plt.title('Regular and II Loss')
+    # Add in a title and axes labels
+    plt.title('Regular and II Loss')
+else:
+    plt.title("Regular Loss")
+
 plt.xlabel('Steps')
 plt.ylabel('Loss')
  
